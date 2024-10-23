df6c7fafd371948bb0a85f6410242d55424db2f6
Andy Carter <andycarter@Andys-MacBook-Pro.local>
2024-10-23 15:17:48 +0100

    remove kivymd ref

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from kivymd.app import MDApp
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
 
 GUI = Builder.load_file('main.kv')
 
